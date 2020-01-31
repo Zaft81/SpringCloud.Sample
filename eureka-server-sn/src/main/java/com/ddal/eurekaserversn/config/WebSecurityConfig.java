792e7b0224ec00d1874041afdc683cfd488e127c
Zaft81 <Ryan81@126.com>
2020-01-31 14:50:46 +0800

    update

---
+++ eureka-server-sn/src/main/java/com/ddal/eurekaserversn/config/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.ddal.eurekaserversn.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * @author Ryan
+ * @version 1.0
+ * @date 1/31/2020
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * 高版本springcloud的丢弃了配置:
+     *
+     * security:
+     *   basic:
+     *    enabled: true
+     *
+     * 所以，用以下方式开启
+     *
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        http.csrf().disable();
+        //注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,
+        //如果是form方式,不能使用url格式登录
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        //super.configure(http);
+    }
+}
