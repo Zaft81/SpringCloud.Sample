dcfd65ed9036ee8f7d6c9c9819fcaf15e40eae24
Zaft81 <Ryan81@126.com>
2020-02-07 20:37:52 +0800

    update

--- shop-order/src/main/java/com/ddal/shoporder/feign/ShopItemFeignClient.java
+++ shop-order/src/main/java/com/ddal/shoporder/feign/ShopItemFeignClient.java
@@ -2,6 +2,7 @@ package com.ddal.shoporder.feign;
 
 import com.ddal.shoporder.entity.ShopItem;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,5 +13,5 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public interface ShopItemFeignClient {
 
     @RequestMapping(value = "/shopitem/{id}",method = RequestMethod.GET)
-    ShopItem queryShopItemById(String id);
+    ShopItem queryShopItemById(@PathVariable("id") String id);
 }
